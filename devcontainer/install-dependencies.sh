70246236438dd8f5763db3479655478c131a4be1
danilson33 <149307417+danilson33@users.noreply.github.com>
2023-12-20 22:03:50 +0100

    Add devcontainer folder

---
+++ devcontainer/install-dependencies.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+pip3 install --user -r scripts/requirements.txt --trusted-host artifactory.myhrtg.net --trusted-host pypi.org --trusted-host pypi.python.org --trusted-host files.pythonhosted.org
